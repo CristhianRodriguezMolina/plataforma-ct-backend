043b368e5c2497a93d72be9e6288197a52ff866c
CristhianRodriguezMolina <50219561+CristhianRodriguezMolina@users.noreply.github.com>
2021-06-05 16:30:03 -0500

    Protecting all remaining routes

--- src/routes/course.routes.js
+++ src/routes/course.routes.js
@@ -4,21 +4,21 @@ const router = Router();
 
 //API modules imports
 import * as courseCtrl from '../controllers/course.controller';
-//import { authJwt } from '../middlewares'
+import { authJwt } from '../middlewares';
 
 // Route for get the courses of a teacher
-router.get('/mycourses/:id', courseCtrl.getMyCourses);
+router.get('/mycourses/:id', [authJwt.verifyToken], courseCtrl.getMyCourses);
 
 // Route for get a course by id
-router.get('/:id', courseCtrl.getCoursesById);
+router.get('/:id', [authJwt.verifyToken], courseCtrl.getCoursesById);
 
 // Route for create a course
-router.post('/', courseCtrl.createCourse);
+router.post('/', [authJwt.verifyToken, authJwt.isAdminOrTeacher], courseCtrl.createCourse);
 
 // Route for delete a course
-router.delete('/:id', courseCtrl.deleteCourse);
+router.delete('/:id', [authJwt.verifyToken, authJwt.isAdminOrTeacher], courseCtrl.deleteCourse);
 
 // Route for update a course
-router.put('/:id', courseCtrl.updateCourseById);
+router.put('/:id', [authJwt.verifyToken, authJwt.isAdminOrTeacher], courseCtrl.updateCourseById);
 
 export default router;
