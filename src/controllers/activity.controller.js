45e250b45c2fb94a46c53473a02575e6dc881094
Wilmar Stiven Valencia Cardona <wilmar2545@gmail.com>
2021-06-12 09:58:58 -0500

    All functions in activity, sequence card and data controller has been surrounded by try catch (Error Proofing updated)

--- src/controllers/activity.controller.js
+++ src/controllers/activity.controller.js
@@ -6,111 +6,180 @@ import Activity from '../models/Activity';
 import * as logicSequenceCtrl from './logic-sequence.controller';
 
 //Get all activities from DB
-export const getActivities = async (req, res) => {
-	await Activity.find(async (err, activities) => {
-		if (err) {
-			console.log("ERROR when we trying to get all activities");
-			console.log(err);
-			return res.status(500).json({ message: "Unexpected error, try again later!" });
-		}
-		await Activity.countDocuments((error, count) => {
-			if (error) {
+export const getActivities = (req, res) => {
+	try {
+		Activity.find((err, activities) => {
+			if (err) {
 				console.log("ERROR when we trying to get all activities");
-				console.log(error);
-				return res.status(500).json({ message: "Unexpected error, try again later!" })
+				console.log(err);
+				return res.status(500).json({ message: "Unexpected error, try again later!" });
 			}
-			return res.status(200).json({ message: "Activities list request has been completed satisfactorily", activities, count });
-		});
+			Activity.countDocuments((error, count) => {
+				if (error) {
+					console.log("ERROR when we trying to get all activities");
+					console.log(error);
+					return res.status(500).json({ message: "Unexpected error, try again later!" })
+				}
+				return res.status(200).json({ message: "Activities list request has been completed satisfactorily", activities, count });
+			});
 
-	});
+		});
+	} catch (e) {
+		console.log(e)
+		return res.status(500).json({ message: "Unexpected error, please try again later!" });
+	}
 
 };
 
 export const getMyActivities = (req, res) => {
-	Activity.find({ creator: req.params.creatorId }, null, { sort: { name: 1 } },
-		(err, activities) => {
-			if (err) return res.status(500).json({ message: "Unexpected error, try again later!" });
-			return res.status(200).json({ message: "Activities list request has been completed satisfactorily", activities });
-		});
+	try {
+		Activity.find({ creator: req.params.creatorId }, null, { sort: { name: 1 } },
+			(err, activities) => {
+				if (err) return res.status(500).json({ message: "Unexpected error, try again later!" });
+				return res.status(200).json({ message: "Activities list request has been completed satisfactorily", activities });
+			});
+	} catch (e) {
+		console.log(e)
+		return res.status(500).json({ message: "Unexpected error, please try again later!" });
+	}
 };
 
 //Create a new activity
-export const createActivity = async (req, res) => {
-	const { name, description, type, creator } = req.body;
+export const createActivity = (req, res) => {
+	try {
+		const { name, description, type, creator } = req.body;
 
-	//Verifying Fields
-	if (!name) {
-		return res.status(400).json({ message: "Field(s) required!" })
-	}
-	if (type.localeCompare("logic_sequence") != 0 &&
-		type.localeCompare("maze") != 0 &&
-		type.localeCompare("questionnaire")) {
-		return res.status(400).json({ message: "Invalid type" })
-	}
-
-	let temName = name.trim();
-
-	//Verifying Fields
-	if (temName.localeCompare("") == 0) {
-		return res.status(400).json({ message: "Field(s) required!" })
-	}
-
-	//Creating a new activity model
-	const newActivity = new Activity({ name: temName, description, type, creator })
-
-	// Save the activity in the DB
-	await newActivity.save((err, savedActivity) => {
-		if (err) return res.status(500).json({ message: "Unexpected error, try again later!" })
-		let child;
-		if (type.localeCompare("logic_sequence") == 0) {
-			child = logicSequenceCtrl.createLogicSequence(savedActivity._id);
-		} else if (type.localeCompare("maze") == 0) {
-			console.log("maze");
-			//Do some stuff
-		} else if (type.localeCompare("questionnaire") == 0) {
-			console.log("questionnaire");
-			//Do some stuff
+		//Verifying Fields
+		if (!name) {
+			return res.status(400).json({ message: "Field(s) required!" })
+		}
+		if (type.localeCompare("logic_sequence") != 0 &&
+			type.localeCompare("maze") != 0 &&
+			type.localeCompare("questionnaire")) {
+			return res.status(400).json({ message: "Invalid type" })
 		}
 
-		if (child) {
-			child.then(() => {
-				return res.status(201).json({ message: "The activity has been created satisfactorily", activity_id: savedActivity._id });
-			}).catch(err => {
-				console.log("ERROR found in createLogicSequence(logic_sequence.controller)")
-				console.error(err);
-				return res.status(500).json({ message: "Unexpected error, try again later!" })
-			});
+		let temName = name.trim();
+
+		//Verifying Fields
+		if (temName.localeCompare("") == 0) {
+			return res.status(400).json({ message: "Field(s) required!" })
 		}
-		else {
-			return res.status(400).json({ message: "Type no accepted" });
-		}
-	});
+
+		//Creating a new activity model
+		const newActivity = new Activity({ name: temName, description, type, creator })
+
+		// Save the activity in the DB
+		newActivity.save((err, savedActivity) => {
+			if (err) return res.status(500).json({ message: "Unexpected error, try again later!" })
+			let child;
+			if (type.localeCompare("logic_sequence") == 0) {
+				child = logicSequenceCtrl.createLogicSequence(savedActivity._id);
+			} else if (type.localeCompare("maze") == 0) {
+				console.log("maze");
+				//Do some stuff
+			} else if (type.localeCompare("questionnaire") == 0) {
+				console.log("questionnaire");
+				//Do some stuff
+			}
+
+			if (child) {
+				child.then(() => {
+					return res.status(201).json({ message: "The activity has been created satisfactorily", activity_id: savedActivity._id });
+				}).catch(err => {
+					console.log("ERROR found in createLogicSequence(logic_sequence.controller)")
+					console.error(err);
+					return res.status(500).json({ message: "Unexpected error, try again later!" })
+				});
+			}
+			else {
+				return res.status(400).json({ message: "Type no accepted" });
+			}
+		});
+	} catch (e) {
+		console.log(e)
+		return res.status(500).json({ message: "Unexpected error, please try again later!" });
+	}
 };
 
 //Update an activity
-export const updateActivityById = async (req, res) => {
+export const updateActivityById = (req, res) => {
 
-	const { name } = req.body.activity;
+	try {
+		const { name } = req.body.activity;
 
-	if (!name) {
-		return res.status(400).json({ message: "Field(s) required!" })
-	}
-
-	let temName = name.trim();
-
-	//Verifying Fields
-	if (temName.localeCompare("") == 0) {
-		return res.status(400).json({ message: "Field(s) required!" })
-	}
-
-	await Activity.findById(req.params.id, (err, activity) => {
-		if (err) {
-			return res.status(500).json({ message: "Unexpected error, try again later!" });
+		if (!name) {
+			return res.status(400).json({ message: "Field(s) required!" })
 		}
-		if (activity) {
+
+		let temName = name.trim();
+
+		//Verifying Fields
+		if (temName.localeCompare("") == 0) {
+			return res.status(400).json({ message: "Field(s) required!" })
+		}
+
+		Activity.findById(req.params.id, (err, activity) => {
+			if (err) {
+				return res.status(500).json({ message: "Unexpected error, try again later!" });
+			}
+			if (activity) {
+				let child;
+				if (activity.type.localeCompare("logic_sequence") == 0) {
+					child = logicSequenceCtrl.updateLogicSequenceByActivityId(activity._id, req.body.child);
+				} else if (activity.type.localeCompare("maze") == 0) {
+					console.log("maze");
+					//Do some stuff
+				} else if (activity.type.localeCompare("questionnaire") == 0) {
+					console.log("questionnaire");
+					//Do some stuff
+				}
+				if (!child) return res.status(500).json({ message: "Unexpected error, try again later!" });
+				child.then(() => {
+					Activity.findByIdAndUpdate(req.params.id, req.body.activity, {
+						new: true
+					}).then(() => {
+						return res.status(201).json({ message: "The activity has been updated satisfactorily" });
+					}).catch(error => {
+						console.log("ERROR found in updateActivityById(activity.controller)");
+						console.error(error);
+						return res.status(500).json({ message: "Unexpected error, try again later!" });
+					})
+				}).catch(e => {
+					console.log("ERROR found in updateLogicSequenceByActivityId(logic_sequence.controller)");
+					console.error(e);
+					return res.status(500).json({ message: "Unexpected error, try again later!" });
+				});
+
+			}
+			else {
+				return res.status(400).json({ message: "Activity not found" });
+			}
+		});
+
+	} catch (e) {
+		console.log(e)
+		return res.status(500).json({ message: "Unexpected error, please try again later!" });
+	}
+
+};
+
+//Delete an activity
+export const deleteActivityById = (req, res) => {
+
+	try {
+		Activity.findById(req.params.id, async (err, activity) => {
+			if (err) {
+				return res.status(500).json({ message: "Unexpected error, try again later!" });
+			}
+
+			if (!activity) {
+				return res.status(400).json({ message: "Activity not found" });
+			}
+
 			let child;
 			if (activity.type.localeCompare("logic_sequence") == 0) {
-				child = logicSequenceCtrl.updateLogicSequenceByActivityId(activity._id, req.body.child);
+				child = logicSequenceCtrl.deleteLogicSequenceByActivityId(activity._id);
 			} else if (activity.type.localeCompare("maze") == 0) {
 				console.log("maze");
 				//Do some stuff
@@ -118,60 +187,24 @@ export const updateActivityById = async (req, res) => {
 				console.log("questionnaire");
 				//Do some stuff
 			}
-			if (!child) return res.status(500).json({ message: "Unexpected error, try again later!" });
-			child.then(async () => {
-				await Activity.findByIdAndUpdate(req.params.id, req.body.activity, {
-					new: true
-				}).then(() => {
-					return res.status(201).json({ message: "The activity has been updated satisfactorily" });
-				}).catch(error => {
-					console.log("ERROR found in updateActivityById(activity.controller)");
-					console.error(error);
-					return res.status(500).json({ message: "Unexpected error, try again later!" });
-				})
-			}).catch(e => {
-				console.log("ERROR found in updateLogicSequenceByActivityId(logic_sequence.controller)");
-				console.error(e);
-				return res.status(500).json({ message: "Unexpected error, try again later!" });
-			});
 
-		}
-		else {
-			return res.status(400).json({ message: "Activity not found" });
-		}
-	});
+			if (child) {
+				child.then(() => {
+					Activity.deleteOne({ _id: activity._id }, (error) => {
+						if (error) {
+							console.error("ERROR when try to deleteOne in deleteActivityById (activity.controller)");
+							return res.status(500).json({ message: "Unexpected error, try again later!" })
+						}
 
-
-};
-
-//Delete an activity
-export const deleteActivityById = async (req, res) => {
-
-	await Activity.findById(req.params.id, async (err, activity) => {
-		if (err) {
-			// console.log("ERROR when try to findById in deleteActivityById (activity.controller)");
-			// console.error(err);
-			return res.status(500).json({ message: "Unexpected error, try again later!" });
-		}
-
-		if (!activity) {
-			return res.status(400).json({ message: "Activity not found" });
-		}
-
-		let child;
-		if (activity.type.localeCompare("logic_sequence") == 0) {
-			child = logicSequenceCtrl.deleteLogicSequenceByActivityId(activity._id);
-		} else if (activity.type.localeCompare("maze") == 0) {
-			console.log("maze");
-			//Do some stuff
-		} else if (activity.type.localeCompare("questionnaire") == 0) {
-			console.log("questionnaire");
-			//Do some stuff
-		}
-
-		if (child) {
-			child.then(async () => {
-				await Activity.deleteOne({ _id: activity._id }, (error) => {
+						return res.status(200).json({ message: "The activity has been deleted satisfactorily" });
+					});
+				}).catch((e) => {
+					console.log("ERROR found in deleteActivityById(activity.controller)")
+					console.error(e);
+					return res.status(500).json({ message: "Unexpected error, try again later!" })
+				});
+			} else {
+				Activity.deleteOne({ _id: activity._id }, (error) => {
 					if (error) {
 						console.error("ERROR when try to deleteOne in deleteActivityById (activity.controller)");
 						return res.status(500).json({ message: "Unexpected error, try again later!" })
@@ -179,22 +212,10 @@ export const deleteActivityById = async (req, res) => {
 
 					return res.status(200).json({ message: "The activity has been deleted satisfactorily" });
 				});
-			}).catch((e) => {
-				console.log("ERROR found in deleteActivityById(activity.controller)")
-				console.error(e);
-				return res.status(500).json({ message: "Unexpected error, try again later!" })
-			});
-		} else {
-			await Activity.deleteOne({ _id: activity._id }, (error) => {
-				if (error) {
-					console.error("ERROR when try to deleteOne in deleteActivityById (activity.controller)");
-					return res.status(500).json({ message: "Unexpected error, try again later!" })
-				}
-
-				return res.status(200).json({ message: "The activity has been deleted satisfactorily" });
-			});
-		}
-	});
-
-
+			}
+		});
+	} catch (e) {
+		console.log(e)
+		return res.status(500).json({ message: "Unexpected error, please try again later!" });
+	}
 };
