c95f6b2cad9aa47b44d9dcdfb010ca17fb4c9208
Wilmar Stiven Valencia Cardona <wilmar2545@gmail.com>
2021-06-16 11:57:10 -0500

    synch

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -6,8 +6,10 @@ import { v4 as uuidv4 } from 'uuid';
 
 // Image upload settings
 const storageImg = multer.diskStorage({
-    destination: function(req, file, cb){
+    destination: function (req, file, cb) {
         const folderPath = path.join(__dirname, '../../static_content/i')
+        console.log('folderPath');
+        console.log(folderPath);
         if (!fs.existsSync(folderPath)) {
             mkdirp(folderPath).then(made => {
                 console.log('Made directory, starting with ', made);
@@ -15,7 +17,7 @@ const storageImg = multer.diskStorage({
             })
         }
         return cb(null, folderPath) //Image destination for images
-    }, 
+    },
     filename: (req, file, cb) => {
         return cb(null, uuidv4() + path.extname(file.originalname).toLowerCase()); //Uploaded file name for images
     }
