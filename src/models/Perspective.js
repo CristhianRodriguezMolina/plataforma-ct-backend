5a76b54c718dd001dd49e564102a0dafb126b9b6
CristhianRodriguezMolina <50219561+CristhianRodriguezMolina@users.noreply.github.com>
2021-09-02 19:22:08 -0500

    Prespective schema

---
+++ src/models/Perspective.js
@@ -0,0 +1,13 @@
+import mongoose, { Schema, model } from 'mongoose';
+
+const PerspectiveSchema = new Schema({
+	'course': { ref: 'Course', type: Schema.Types.ObjectId },
+	'student': { ref: 'Person', type: Schema.Types.ObjectId },
+	'teacher': { ref: 'Person', type: Schema.Types.ObjectId },
+	'course_name': { type: String, require: true },
+	'course_description': { type: String, require: true },
+	'teacher_name': { type: String, require: true },
+	'message': { type: String, require: true },
+}, { timestamps: true });
+
+export default model('Prespective', PerspectiveSchema);
